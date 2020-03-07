6b7562faa730e6f5ab893a564717564ab48a1e6a
jeng-linux <jamespaulespea@gmail.com>
2020-03-08 02:04:51 +0800

    Testing phase3

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo
 echo -e "Hello \e[5m\e[32m$(whoami)\e[39m\e[25m I need to run update some packages & scripts that is why \e[31mSudo\e[39m is needed:"
-sudo bash grab.sh
+sudo sh ./grab.sh
