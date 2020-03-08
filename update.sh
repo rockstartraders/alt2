fde670cac238fea22cfd5913e924a1cd72cd9ecd
jeng-linux <jamespaulespea@gmail.com>
2020-03-08 17:07:56 +0800

    fix statement sting 2

--- update.sh
+++ update.sh
@@ -7,8 +7,7 @@ function changeD() {
 }
 
 function fetch() {
-    sudo svn export --force https://github.com/rockstartraders/alt2/trunk/parser.sh > /dev/null
-    chmod 777 parser.sh
+    svn export --force https://github.com/rockstartraders/alt2/trunk/parser.sh > /dev/null && chmod 777 parser.sh
 }
 
 
@@ -16,10 +15,12 @@ function parser() {
     sh ./parser.sh
 }
 
+bash << EOF
+  echo
+  changeD
+  echo -e "Hello \e[5m\e[32m$(whoami)\e[39m\e[25m I need to run update some packages & scripts that is why \e[31mSudo\e[39m is needed:"
+  echo
+  fetch
+  echo "done"
 
-echo
-changeD
-echo -e "Hello \e[5m\e[32m$(whoami)\e[39m\e[25m I need to run update some packages & scripts that is why \e[31mSudo\e[39m is needed:"
-echo
-fetch
-echo "done"
+EOF
