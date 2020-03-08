58973f566c04263b7f8f559ae0b37d024a9ea663
jeng-linux <jamespaulespea@gmail.com>
2020-03-08 16:52:43 +0800

    chmod 777 part2

--- update.sh
+++ update.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 function changeD() {
+
    cd ~
    cd /alt2/
 }
 
 function fetch() {
     svn export --force https://github.com/rockstartraders/alt2/trunk/parser.sh > /dev/null
+    chmod 777 parser.sh
 }
 
 
 function parser() {
-    bash./parser.sh
+    sh ./parser.sh
 }
 
 
 echo
 changeD
 echo -e "Hello \e[5m\e[32m$(whoami)\e[39m\e[25m I need to run update some packages & scripts that is why \e[31mSudo\e[39m is needed:"
-sudo bash
 echo
 fetch
-chmod 777 parser.sh
-parser
+sh./parser.sh
