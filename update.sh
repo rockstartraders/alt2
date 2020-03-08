e34dc9a35e731e85f9e14231064b0ebfd05e6593
jeng-linux <jamespaulespea@gmail.com>
2020-03-08 16:43:29 +0800

    chmod 777

--- update.sh
+++ update.sh
@@ -6,18 +6,20 @@ function changeD() {
 }
 
 function fetch() {
-    sudo svn export --force https://github.com/rockstartraders/alt2/trunk/parser.sh > /dev/null && chmod 777 parser.sh
+    svn export --force https://github.com/rockstartraders/alt2/trunk/parser.sh > /dev/null
 }
 
 
 function parser() {
-    sh ./parser.sh
+    bash./parser.sh
 }
 
 
 echo
 changeD
 echo -e "Hello \e[5m\e[32m$(whoami)\e[39m\e[25m I need to run update some packages & scripts that is why \e[31mSudo\e[39m is needed:"
+sudo bash
 echo
 fetch
+chmod 777 parser.sh
 parser
